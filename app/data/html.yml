0e519464f8d20901ad4721c7e00810f6a99b886e
Ashley Williams <ashley@bocoup.com>
2014-12-18 18:54:07 -0500

    set up app to have dynamic routes based on yml topic files in a data folder, with 2 example files

---
+++ app/data/html.yml
@@ -0,0 +1,2 @@
+title: Hypertext Markup Language, or HTML
+content: What is HTML? I can't even.
--- app/index.js
+++ app/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
 const express = require('express');
 const app = express();
 
@@ -5,6 +7,17 @@ app.use(express.static('public'));
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
+
+app.get('/topics/:topic', function(req, res){
+  try {
+    var topic_doc = fs.readFileSync('app/data/' + req.params.topic + '.yml', 'utf8');
+    var topic_data = yaml.safeLoad(topic_doc);
+    console.log(topic_data);
+  } catch(e) {
+    console.log(e);
+  }
+  res.render('topic', topic_data);
+});
 
 app.get('/', function(req, res){
   res.render('index');
